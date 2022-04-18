e2af3acd278141752cabc50d8f1f305571fc6e36
jack13only <ferretreva@gmail.com>
2022-04-18 13:08:51 +0300

    init: init commit

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+import React from 'react';
+import './index.scss';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
