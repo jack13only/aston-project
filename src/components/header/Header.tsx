a761b941f34318eaa7145cd967e94a6432085eb3
jack13only <ferretreva@gmail.com>
2022-04-29 15:34:30 +0300

    feat: add responsive design 2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,10 @@ const Header = (): JSX.Element => {
   return (
     <div className="header">
       <Link to={PATHS.main} className="header__link">
-        <h1 className="header__title">Rick and Morty</h1>
+        <h1 className="header__title">
+          <span className="header__title-small">RiM</span>
+          <span className="header__title-big">Rick and Morty</span>
+        </h1>
       </Link>
 
       {isAuthenticated && (
