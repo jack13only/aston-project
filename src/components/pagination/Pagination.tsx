0272920f6e07c0fcb282d4508d3bc0f734626fd0
jack13only <ferretreva@gmail.com>
2022-04-29 14:15:24 +0300

    fix: fix warnings

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import './Pagination.scss';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
